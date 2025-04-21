c3aa8f1c829266bc706283bf566a0b7266d75183
franck1733 <fsottou@yahoo.fr>
2025-04-21 10:56:33 +0200

    Create MicrocementForm.js

---
+++ src/components/MicrocementForm.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const MicrocementForm = () => {
+  return (
+    <div className="text-center p-8">
+      <h1 className="text-2xl font-bold">Microcement Questionnaire</h1>
+      <p>This is where your form will go.</p>
+    </div>
+  );
+};
+
+export default MicrocementForm;
