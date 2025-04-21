60dba414fe790e072fc4923fa580ffcbc716cb40
franck1733 <fsottou@yahoo.fr>
2025-04-21 10:51:23 +0200

    Create App.js

---
+++ src/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import MicrocementForm from "./components/MicrocementForm";
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 p-4">
+      <MicrocementForm />
+    </div>
+  );
+}
+
+export default App;
